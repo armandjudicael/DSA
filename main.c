a5ba1e392a19e9873a30690630a974da3ad88497
kael <judicael.ratombotiana@gmail.com>
2023-01-02 16:36:31 +0300

    First commit on DSA

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <malloc.h>
+int age = 0;
+int main(){
+
+    printf(" Veuillez entrer votre age : ");
+
+    scanf("%d",&age);
+
+    printf(" Votre age est %d  ",age);
+
+   return 0;
+}
